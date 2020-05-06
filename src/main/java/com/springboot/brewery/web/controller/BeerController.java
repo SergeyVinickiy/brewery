90a84ab789a94413fd753bb42f0e9aeb6dab1a4e
SergeyV1 <sergey.vinickiy@verifone.com>
2020-05-06 22:46:10 +0300

    adding beer service

--- src/main/java/com/springboot/brewery/web/controller/BeerController.java
+++ src/main/java/com/springboot/brewery/web/controller/BeerController.java
@@ -1,22 +1,30 @@
 package com.springboot.brewery.web.controller;
 
 
+import com.springboot.brewery.services.BeerService;
 import com.springboot.brewery.web.model.BeerDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.UUID;
 
-@RestController("/api/v1/beer")
+@RequestMapping("/api/v1/beer")
+@RestController
 public class BeerController {
 
-    @GetMapping({"/{beerId}"})
-    public ResponseEntity getBeer(@PathVariable("beerId") UUID beerId){
+    private final BeerService beerService;
 
-        return new ResponseEntity<>(BeerDto.builder().build(),HttpStatus.OK );
+    public BeerController(BeerService beerService) {
+        this.beerService = beerService;
+    }
+
+    @GetMapping({"/{beerId}"})
+    public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId){
+
+        return new ResponseEntity<>(beerService.getBeerById(beerId),HttpStatus.OK );
 
     }
 
