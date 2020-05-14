e55c1bba7b66265218e0127f269b4857e94ef533
SergeyV1 <sergey.vinickiy@verifone.com>
2020-05-14 21:19:40 +0300

    add validation to customer

--- src/main/java/com/springboot/brewery/web/model/CustomerDto.java
+++ src/main/java/com/springboot/brewery/web/model/CustomerDto.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Null;
 import java.util.UUID;
 
 @Data
@@ -13,7 +17,13 @@ import java.util.UUID;
 @Builder
 public class CustomerDto {
 
+    @Null
     private UUID id;
+
+    @Null
+    @NotBlank
+    @Min(3)
+    @Max(100)
     private String customerName;
 
 }
